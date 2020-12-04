198c1fbe659701cba1a747e9d877b712c78e651b
EpicDoomSir <eitands@gmail.com>
2020-12-04 15:46:32 -0500

    added destroy all in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,15 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.destroy_all
+Game.destroy_all
+GameSession.destroy_all
 
 user1 = User.create(username: "bob", pin: 1234, total_points: 0, avatar: "https://scrumandkanban.co.uk/wp-content/uploads/2013/07/DavidLowe.jpg")
 user2 = User.create(username: "taylor", pin: 4321, total_points: 0, avatar: "https://cdn.dribbble.com/users/199982/screenshots/4044699/furkan-avatar-dribbble.png")
 user3 = User.create(username: "beth", pin: 1234, total_points: 0, avatar: "https://o0o0sm3y25-flywheel.netdna-ssl.com/wp-content/uploads/2019/08/7-Best-Sites-To-Create-Avatars-Of-Yourself.jpg")
 
 game1 = Game.create(title: "memory", time: 120)
+game1 = Game.create(title: "sliding", time: 200)
 
 GameSession.create(user_id: user1.id, game_id: game1.id, score: 60)
 GameSession.create(user_id: user2.id, game_id: game1.id, score: 90)
